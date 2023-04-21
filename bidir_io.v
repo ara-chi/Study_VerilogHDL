fc2ad4eb826302a465341323e65c4febee8699d5
ara-chi <109021761+ara-chi@users.noreply.github.com>
2023-04-22 01:38:44 +0900

    Update bidir_io.v

--- bidir_io.v
+++ bidir_io.v
@@ -1,13 +1,13 @@
-module bidir_io #(parameter WIDTH=1) (
-  input  wire           t , // Control signal for driving the inout port
-  inout  wire [WIDTH-1] i , // Input port
-  output wire [WIDTH-1] o , // Output port
-  inout  wire [WIDTH-1] io  // Inout port
+module bidir_io (
+  input  wire t , // Control signal for driving the inout port
+  inout  wire i , // Input port
+  output wire o , // Output port
+  inout  wire io  // Inout port
 );
   
   // Assign either input or high impedance to inout port based on control signal
   // If t is 1, assign input to inout port, else if t is 0, assign high impedance to inout port
-  assign io = t ? i : {WIDTH{1'bz}};
+  assign io = t ? i : 1'bz;
   
   // Assign the value of the inout port to the output port
   assign o = io;
